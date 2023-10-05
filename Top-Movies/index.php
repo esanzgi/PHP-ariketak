0541eaa12a87c43508abcc8e91907cb0d68ed6ec
esanzgi <esanzgi22wg@ikzubirimanteo.com>
2023-10-05 09:56:13 +0200

    Top-Movie hasita

---
+++ Top-Movies/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="main.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" placeholder="Erabiltzailea" required>
+
+        <br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
