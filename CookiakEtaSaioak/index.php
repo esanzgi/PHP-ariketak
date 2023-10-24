2dbe8d5fca73cf25ac9ffe20034bf81fe371c46b
esanzgi <esanzgi22wg@ikzubirimanteo.com>
2023-10-24 07:59:28 +0200

    .gitignore gehituta

---
+++ CookiakEtaSaioak/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Web Txikia</title>
+</head>
+<body>
+  <h2>Formularioa 1</h2>
+  <form action="">
+    <label for="koloreak">Koloreak</label>
+    <br><br>
+    <select name="koloreak" id="">
+      <option value="blue" selected>Urdina</option>
+      <option value="red">Gorria</option>
+      <option value="green">Berdea</option>
+      <option value="yellow">Horia</option>
+    </select>
+    <br><br>
+    <button type="submit">Bidali</button>
+  </form>
+
+  <br><br>
+  <h2>Formularioa 2</h2>
+  <form action="">
+    <label for="erabiltzailea"></label>
+    <input type="text" name="erabiltzailea" placeholder="Erabiltzailea">
+  </form>
+</body>
+</html>
